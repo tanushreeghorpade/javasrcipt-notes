1c9cac34071816c949141ff3523d8e50c00f2733
Tanushree Ghorpade <166472953+tanushreeghorpade@users.noreply.github.com>
2024-11-27 16:29:01 +0000

    string files

---
+++ Practice/String.js
@@ -0,0 +1,21 @@
+const name1 = "Tanushree";
+const name2 = "Ghorpade"
+// right method to concatinate string 
+console.log(`Hii my name is ${name1} Rajendra ${name2} `);
+
+console.log(name2.__proto__ );
+console.log(name1.at(2));//character of that index 
+console.log(name2.length);//number of character
+console.log(name1.includes("e",-1));//character is placed on same position or not
+console.log(name2.slice(2,5));
+console.log(name1.split());
+console.log(name2.replace());
+
+
+
+
+
+
+
+
+
